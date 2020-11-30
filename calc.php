525ec9b0aea8906a61227bf9725f86b7789b0117
babahus <dp101196@gmail.com>
2020-11-30 19:46:20 +0200

    add calc add comment for bad operation add elseif

--- calc.php
+++ calc.php
@@ -4,40 +4,38 @@ error_reporting(E_ERROR);
 /*check post request and make math*/
 if (isset($_POST['submit'])) {
     $number1 = $_POST['number1'];
-    if (!isset($_POST['number2']) && $_POST['operation'] === 'radical' OR isset($_POST['number2']) && $_POST['operation'] === 'radical'){
+    $number2 = $_POST['number2'];
+    if (!isset($_POST['number2']) && $_POST['operation'] === 'radical'
+        or isset($_POST['number2']) && $_POST['operation'] === 'radical') {
         $result = sqrt($number1);
         echo $result;
-    }
-
-    $number2 = $_POST['number2'];
-    if ($_POST['operation'] === 'plus') {
+    } elseif ($_POST['operation'] === 'plus') {
         $result = $number1 + $number2;
         echo $result;
-    }
-    
-    if ($_POST['operation'] === 'minus') {
+    } elseif ($_POST['operation'] === 'minus') {
         $result = $number1 - $number2;
         echo $result;
-    }
-
-    if ($_POST['operation'] === 'multiply') {
+    } elseif ($_POST['operation'] === 'multiply') {
         $result = $number1 * $number2;
         echo $result;
+    } elseif ($_POST['operation'] === 'scrt') {
+        $result = $number1 ** $number2;
+        echo $result;
+    } elseif ($_POST['operation'] === 'divide' && $number2 != '0') {
+        $result = $number1 / $number2;
+        echo $result;
+    } else {
+        echo "Bad operation";
     }
+}
 
+/*if (isset($_POST['submit'])) {
     if ($_POST['operation'] === 'divide' && $number2 != '0') {
         $result = $number1 / $number2;
         echo $result;
     } else {
-            echo "Bad operation";
-            exit();
-
+        exit("Bad operation");
     }
-
-    if ($_POST['operation'] === 'scrt') {
-        $result = $number1 ** $number2;
-        echo $result;
-    }
-}
+}*/
 
 
